0bc1aa0b769a2693f7a690649c20bbf3a74f4c22
irvingr <irvingr@us.ibm.com>
2017-05-19 08:38:21 +0900

    add: script to write data to ucd component version properties for files that were uploaded

---
+++ ucd-set-version-properties.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+url="http://citi.deploy:8080"
+
+/home/vagrant/udclient/udclient -username admin -password admin -weburl $url \
+setVersionProperty -component acme-bank -version v2.6 -name change.type -value "major"
+
+/home/vagrant/udclient/udclient -username admin -password admin -weburl $url \
+setVersionProperty -component acme-bank -version v2.6 -name apic.oldProduct -value "acme-bank:1.0.0"
+
